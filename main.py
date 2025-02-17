30bf8e484248a2c51c11964b52eac02ad3edc063
birajstha:construction_worker::penguin <birajstha@gmail.com>
2025-02-17 18:34:49 -0500

    adding resource-name

--- main.py
+++ main.py
@@ -13,12 +13,16 @@ from colorama import Fore, Style, init
 parser = argparse.ArgumentParser(description="Process CPAC output and generate QC.")
 parser.add_argument("--cpac_output_dir", required=True, help="Path to the CPAC output directory")
 parser.add_argument("--qc_dir", required=True, help="Path to the QC output directory")
+parser.add_argument("--overlay_csv", required=False, help="Overlay CSV file")
+parser.add_argument("--n_procs", type=int, default=10, help="Number of processes to use for multiprocessing")
+
 args = parser.parse_args()
-
-qc_dir = os.path.join(args.qc_dir)
-csv_file = os.path.join(qc_dir, "df.csv")
-
 cpac_output_dir = args.cpac_output_dir
+qc_dir = os.path.join(args.qc_dir)
+overlay_csv = args.overlay_csv
+n_procs = args.n_procs
+
+csv_file = os.path.join(qc_dir, "df.csv")
 
 plots_dir = os.path.join(qc_dir, "plots")
 os.makedirs(qc_dir, exist_ok=True)
@@ -29,7 +33,7 @@ if os.path.exists(csv_file):
     df = pd.read_csv(csv_file)
 else:
     # Load in parallel and stream to disk as a Parquet dataset
-    df = bids2table(cpac_output_dir, persistent=True, workers=10).flat
+    df = bids2table(cpac_output_dir, persistent=True, workers=n_procs).flat
     # Save df as CSV
     df.to_csv(csv_file, index=False)
 
@@ -53,7 +57,30 @@ df = df.drop(columns=["json"])
 nii_gz_files = df[df.file_path.str.endswith(".nii.gz")]
 
 # add one column that breaks the file_path to the last name of the file and drops extension
-nii_gz_files["file_name"] = nii_gz_files.file_path.apply(lambda x: os.path.basename(x).split(".")[0])
+nii_gz_files["file_name"] = nii_gz_files.file_path.apply(lambda x: os.path.basename(x).replace(".nii.gz", ""))
+
+def gen_resource_name(row):
+    sub = row["sub"]
+    ses = row["ses"]
+    task = row["task"] if row["task"] != "" else False
+    run = int(row["run"]) if row["run"] != "" else False
+    
+    scan = f"task-{task}_run-{run}_" if task and run else ""
+    resource_name = row["file_name"].replace(f"sub-{sub}_ses-{ses}_{scan}", "")
+    return resource_name
+
+nii_gz_files["resource_name"] = nii_gz_files.apply(gen_resource_name, axis=1)
+
+# add a utility function to return rows provided a resource_name
+def get_rows_by_resource_name(resource_name):
+    # get all rows that have the resource_name
+    rows = nii_gz_files[nii_gz_files.resource_name == resource_name]
+    if len(rows) == 0:
+        print(Fore.RED + f"NOT FOUND: {resource_name} " + Style.RESET_ALL)
+        return None
+    return rows
+
+    
 
 # check file_path and drop the ones that are higher dimensions for now
 def is_3d(file_path):
@@ -70,29 +97,53 @@ nii_gz_files = nii_gz_files[nii_gz_files.file_path.apply(is_3d)]
 nii_gz_files_csv_path = os.path.join(qc_dir, "nii_gz_files.csv")
 nii_gz_files.to_csv(nii_gz_files_csv_path, index=False)
 
-# Prepare the arguments for each row
-args = [
-    (
-        row['sub'], 
-        row['ses'], 
-        row['datatype'], 
-        row['file_path'], 
-        {
-            "task": row.get('task', ''), 
-            "run": row.get('run', ''),
-            "space": row.get('space', ''),
-            "reg": row.get('reg', ''), 
-            "file_name": row["file_name"],
-            "plots_dir": plots_dir
-        }
-    ) 
-    for _, row in nii_gz_files.iterrows()
-]
+# for rows in overlay_csv find the resource_name and get the rows
+if overlay_csv:
+    overlay_df = pd.read_csv(overlay_csv)
+    overlay_df = overlay_df.fillna(False)
 
-def process_row(args):
-    run(*args[:4], **args[4])
+    def process_row(row):
+        image_1 = row.get("image_1", False)
+        image_2 = row.get("image_2", False)
 
-# Use multiprocessing to process each row with 100 processes
-with Pool(processes=100) as pool:
-    for _ in tqdm(pool.imap_unordered(process_row, args), total=len(args), desc="Processing"):
-        pass
+        print(image_1, image_2)
+
+        # get the resource name from the file name
+        if image_1:
+            resource_name_1 = get_rows_by_resource_name(image_1)
+            if image_2:
+                resource_name_2 = get_rows_by_resource_name(image_2)
+            else:
+                resource_name_2 = None
+        else:
+            print(Fore.RED + f"NOT FOUND: {image_1} " + Style.RESET_ALL)
+            return
+    
+    overlay_df.apply(process_row, axis=1)
+
+# # Prepare the arguments for each row
+# args = [
+#     (
+#         row['sub'], 
+#         row['ses'], 
+#         row['datatype'], 
+#         row['file_path'], 
+#         {
+#             "task": row.get('task', ''), 
+#             "run": row.get('run', ''),
+#             "space": row.get('space', ''),
+#             "reg": row.get('reg', ''), 
+#             "file_name": row["file_name"],
+#             "plots_dir": plots_dir
+#         }
+#     ) 
+#     for _, row in nii_gz_files.iterrows()
+# ]
+
+# def process_row(args):
+#     run(*args[:4], **args[4])
+
+# # Use multiprocessing to process each row with 100 processes
+# with Pool(processes=n_procs) as pool:
+#     for _ in tqdm(pool.imap_unordered(process_row, args), total=len(args), desc="Processing"):
+#         pass
---
+++ test.sh
@@ -0,0 +1,13 @@
+
+source ./venv/bin/activate
+
+CPAC_OUTPUT_DIR=/ocean/projects/med220004p/bshresth/vannucci/all_runs/scripts/outputs/AFNI_FSL_strict_noBBR_run2/output/pipeline_cpac_fmriprep-options
+QC_DIR=/ocean/projects/med220004p/bshresth/projects/cpac-qc
+OVERLAY_CSV=/ocean/projects/med220004p/bshresth/projects/cpac-qc/overlay.csv
+
+# Create the QC directory inside the specified QC_DIR
+QC_DIR_PATH="$QC_DIR/QC"
+mkdir -p "$QC_DIR_PATH"
+
+# Run the main.py script with the specified directories
+python main.py --cpac_output_dir "$CPAC_OUTPUT_DIR" --qc_dir "$QC_DIR_PATH" --overlay_csv "$OVERLAY_CSV"
