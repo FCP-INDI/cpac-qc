c5ad7a8031cb73854b67a9474b2b06cc94518c6c
birajstha:construction_worker::penguin <birajstha@gmail.com>
2025-02-24 12:37:37 -0500

    updating bash scripts

--- gen_qc.sh
+++ gen_qc.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # Check if the correct number of arguments is provided
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <CPAC_OUTPUT_DIR> <QC_DIR>"
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <CPAC_OUTPUT_DIR> <QC_DIR> <OVERLAY_CSV> <N_PROCS>"
     exit 1
 fi
 
 # Assign arguments to variables
 CPAC_OUTPUT_DIR=$1
 QC_DIR=$2
+OVERLAY_CSV=$3
+N_PROCS=$4
 
 # Load the virtual environment
 source /ocean/projects/med220004p/bshresth/projects/cpac_dashboard/venv/bin/activate
@@ -18,10 +20,12 @@ QC_DIR_PATH="$QC_DIR/QC"
 mkdir -p "$QC_DIR_PATH"
 
 # Run the main.py script with the specified directories
-python /ocean/projects/med220004p/bshresth/projects/cpac_dashboard/main.py --cpac_output_dir "$CPAC_OUTPUT_DIR" --qc_dir "$QC_DIR_PATH"
+python main.py --cpac_output_dir "$CPAC_OUTPUT_DIR" --qc_dir "$QC_DIR_PATH" --overlay_csv "$OVERLAY_CSV" --n_procs 10
 
 # Copy the index.html, script.js, and styles.css files to the QC directory
-cp /ocean/projects/med220004p/bshresth/projects/cpac_dashboard/index.html "$QC_DIR_PATH"
+cp /ocean/projects/med220004p/bshresth/projects/cpac-qc/index.html "$QC_DIR_PATH"
+cp /ocean/projects/med220004p/bshresth/projects/cpac-qc/script.js "$QC_DIR_PATH"
+cp /ocean/projects/med220004p/bshresth/projects/cpac-qc/styles.css "$QC_DIR_PATH"
 
 # Deactivate the virtual environment
 deactivate
--- test.sh
+++ test.sh
@@ -5,17 +5,4 @@ CPAC_OUTPUT_DIR=/ocean/projects/med220004p/bshresth/vannucci/all_runs/scripts/ou
 QC_DIR=/ocean/projects/med220004p/bshresth/projects/cpac-qc
 OVERLAY_CSV=/ocean/projects/med220004p/bshresth/projects/cpac-qc/overlay.csv
 
-# Create the QC directory inside the specified QC_DIR
-QC_DIR_PATH="$QC_DIR/QC"
-mkdir -p "$QC_DIR_PATH"
-
-# Run the main.py script with the specified directories
-python main.py --cpac_output_dir "$CPAC_OUTPUT_DIR" --qc_dir "$QC_DIR_PATH" --overlay_csv "$OVERLAY_CSV" --n_procs 20
-
-# Copy the index.html, script.js, and styles.css files to the QC directory
-cp /ocean/projects/med220004p/bshresth/projects/cpac-qc/index.html "$QC_DIR_PATH"
-cp /ocean/projects/med220004p/bshresth/projects/cpac-qc/script.js "$QC_DIR_PATH"
-cp /ocean/projects/med220004p/bshresth/projects/cpac-qc/styles.css "$QC_DIR_PATH"
-
-# Deactivate the virtual environment
-deactivate
+bash gen_qc.sh $CPAC_OUTPUT_DIR $QC_DIR $OVERLAY_CSV 10
